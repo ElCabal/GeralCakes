afcb544fcfd06324a83d41303be36b3395d3871d
ElCabal <cabalholguinlufe@gmail.com>
2022-03-13 23:34:46 -0500

    verificaciones 2

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
 import "react-animated-slider/build/horizontal.css";
-/* import Carousel from "../components/Carousel";
- */
+import Carousel from "../components/Carousel";
+
 import Welcome from "../components/Welcome";
 
 const Landing = () => {
@@ -10,7 +10,7 @@ const Landing = () => {
     <div className=" pt-20 text-2xl bg-white flex flex-col  gap-4 text-center">
       <Welcome />
       <h1 className="font-bold uppercase border-t-2">Nuestros Productos</h1>
-      {/* <Carousel /> */}
+      <Carousel />
     </div>
   );
 };
